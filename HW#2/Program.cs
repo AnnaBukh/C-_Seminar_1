7a09d797f20d524e5588610d88bececca4f2626a
ANNA BUKH <anna.buh@bk.ru>
2022-08-09 15:33:11 +0700

    ADD HomeWork##2,4,6,8 Seminar 1

---
+++ HW#2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 2
+// Напишите программу, которая на вход принимает два числа 
+// и выдает какое число больше, а какое меньше 
+
+
+Console.WriteLine("Введите первое число:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int b = Convert.ToInt32(Console.ReadLine());
+        if (a>b)
+         Console.Write($"число {a} больше {b}");
+        else if (a<b)
+         Console.Write($"число {a} меньше {b}");
+    else Console.Write("числа равны друг другу");
---
+++ HW#4/Program.cs
@@ -0,0 +1,19 @@
+﻿// напишите программу, которая принимает на вход 3 числа
+// и выдает максимальное из этих чисел
+
+Console.WriteLine("Введите первое число:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число:");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if (b > a)
+    max = b;
+if (c > max)
+    max = c;
+
+Console.Write($"максимальное число из трех {max}");
+
